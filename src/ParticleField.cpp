20687612f2a529b7e5621c1571df3c0ffd92f1d4
smeyfroi <steve.meyfroidt@gmail.com>
2026-01-20 19:19:34 +0000

    Add parameter overrides to support ofxMarkSynth intent system

--- src/ParticleField.cpp
+++ src/ParticleField.cpp
@@ -1,7 +1,8 @@
-#include "ParticleField.h"
 #include <cmath>
-#include "ofRandomEngine.h"
+
+#include "ParticleField.h"
 #include "ofLog.h"
+#include "ofRandomEngine.h"
 
 namespace ofxParticleField {
 
@@ -20,34 +21,104 @@ void ParticleField::setup(ofFloatColor particleColor_, float field1ValueOffset_,
   particleColor = particleColor_;
   field1ValueOffset = field1ValueOffset_;
   field2ValueOffset = field2ValueOffset_;
-  
+
   drawShader.load();
   updateShader.load();
   initShader.load();
-  
+
   int initialParticleCount = (int)std::pow(2.0f, ln2ParticleCountParameter.get());
   resizeParticles(initialParticleCount);
+}
+
+void ParticleField::setParameterOverrides(const ParameterOverrides& overrides) {
+  if (parameterOverrides.velocityDamping == overrides.velocityDamping &&
+      parameterOverrides.forceMultiplier == overrides.forceMultiplier &&
+      parameterOverrides.maxVelocity == overrides.maxVelocity &&
+      parameterOverrides.particleSize == overrides.particleSize &&
+      parameterOverrides.jitterStrength == overrides.jitterStrength &&
+      parameterOverrides.jitterSmoothing == overrides.jitterSmoothing &&
+      parameterOverrides.speedThreshold == overrides.speedThreshold &&
+      parameterOverrides.minWeight == overrides.minWeight &&
+      parameterOverrides.maxWeight == overrides.maxWeight &&
+      parameterOverrides.field1Multiplier == overrides.field1Multiplier &&
+      parameterOverrides.field2Multiplier == overrides.field2Multiplier) {
+    return;
+  }
+
+  updateOverrides(overrides);
+}
+
+void ParticleField::clearParameterOverrides() {
+  setParameterOverrides(ParameterOverrides {});
+}
+
+void ParticleField::updateOverrides(const ParameterOverrides& overrides) {
+  parameterOverrides = overrides;
+}
+
+float ParticleField::getVelocityDampingEffective() const {
+  return parameterOverrides.velocityDamping.value_or(velocityDampingParameter.get());
+}
+
+float ParticleField::getForceMultiplierEffective() const {
+  return parameterOverrides.forceMultiplier.value_or(forceMultiplierParameter.get());
+}
+
+float ParticleField::getMaxVelocityEffective() const {
+  return parameterOverrides.maxVelocity.value_or(maxVelocityParameter.get());
+}
+
+float ParticleField::getParticleSizeEffective() const {
+  return parameterOverrides.particleSize.value_or(particleSizeParameter.get());
+}
+
+float ParticleField::getJitterStrengthEffective() const {
+  return parameterOverrides.jitterStrength.value_or(jitterStrengthParameter.get());
+}
+
+float ParticleField::getJitterSmoothingEffective() const {
+  return parameterOverrides.jitterSmoothing.value_or(jitterSmoothingParameter.get());
+}
+
+float ParticleField::getSpeedThresholdEffective() const {
+  return parameterOverrides.speedThreshold.value_or(speedThresholdParameter.get());
+}
+
+float ParticleField::getMinWeightEffective() const {
+  return parameterOverrides.minWeight.value_or(minWeightParameter.get());
+}
+
+float ParticleField::getMaxWeightEffective() const {
+  return parameterOverrides.maxWeight.value_or(maxWeightParameter.get());
+}
+
+float ParticleField::getField1MultiplierEffective() const {
+  return parameterOverrides.field1Multiplier.value_or(field1MultiplierParameter.get());
+}
+
+float ParticleField::getField2MultiplierEffective() const {
+  return parameterOverrides.field2Multiplier.value_or(field2MultiplierParameter.get());
 }
 
 void ParticleField::resizeParticles(int newApproxNumParticles) {
   size_t newWidth, newHeight;
   calculateParticleDimensions(newApproxNumParticles, newWidth, newHeight);
   size_t newCount = newWidth * newHeight;
-  
+
   bool isInitialSetup = !particleDataFbo.isAllocated();
-  
+
   if (!isInitialSetup) {
     size_t oldWidth = particleDataFbo.getWidth();
     size_t oldHeight = particleDataFbo.getHeight();
     size_t oldCount = oldWidth * oldHeight;
-    
+
     if (oldWidth == newWidth && oldHeight == newHeight) {
       return;
     }
-    
+
     ofFbo tempFbo;
     tempFbo.allocate(createParticleDataFboSettings(oldWidth, oldHeight));
-    
+
     tempFbo.begin();
     for (size_t i = 0; i < numDataBuffers; ++i) {
       glDrawBuffer(GL_COLOR_ATTACHMENT0 + i);
@@ -56,12 +127,12 @@ void ParticleField::resizeParticles(int newApproxNumParticles) {
     }
     glDrawBuffer(GL_COLOR_ATTACHMENT0); // Reset to default attachment
     tempFbo.end();
-    
+
     particleDataFbo.allocate(createParticleDataFboSettings(newWidth, newHeight));
-    
+
     size_t minWidth = (oldWidth < newWidth) ? oldWidth : newWidth;
     size_t minHeight = (oldHeight < newHeight) ? oldHeight : newHeight;
-    
+
     particleDataFbo.getSource().begin();
     for (size_t i = 0; i < numDataBuffers; ++i) {
       glDrawBuffer(GL_COLOR_ATTACHMENT0 + i);
@@ -71,7 +142,7 @@ void ParticleField::resizeParticles(int newApproxNumParticles) {
       tempFbo.getTexture(i).draw(0, 0, minWidth, minHeight);
     }
     glDrawBuffer(GL_COLOR_ATTACHMENT0); // Reset to default attachment
-    
+
     if (newCount > oldCount) {
       size_t initMinHeight = (oldHeight < newHeight) ? oldHeight : newHeight;
       if (newWidth > oldWidth) {
@@ -84,12 +155,12 @@ void ParticleField::resizeParticles(int newApproxNumParticles) {
     particleDataFbo.getSource().end();
   } else {
     particleDataFbo.allocate(createParticleDataFboSettings(newWidth, newHeight));
-    
+
     particleDataFbo.getSource().begin();
     initializeParticleRegion(0, 0, newWidth, newHeight);
     particleDataFbo.getSource().end();
   }
-  
+
   rebuildMesh(newWidth, newHeight);
 }
 
@@ -100,7 +171,15 @@ void ParticleField::calculateParticleDimensions(int approxNumParticles, size_t& 
 
 void ParticleField::initializeParticleRegion(size_t x, size_t y, size_t width, size_t height) {
   for (size_t i = 0; i < numDataBuffers; ++i) {
-    initShader.initializeRegion(particleDataFbo.getTarget(), i, x, y, width, height, ofRandom(10000.0f, 99999.0f) + i * 123.45f, minWeightParameter, maxWeightParameter);
+    initShader.initializeRegion(particleDataFbo.getTarget(),
+                               i,
+                               x,
+                               y,
+                               width,
+                               height,
+                               ofRandom(10000.0f, 99999.0f) + i * 123.45f,
+                               getMinWeightEffective(),
+                               getMaxWeightEffective());
   }
 }
 
@@ -143,27 +222,39 @@ void ParticleField::update() {
     resizeParticles(pendingParticleCount);
     pendingResize = false;
   }
-  
+
   if (field2Texture.isAllocated() && field1Texture.isAllocated()) {
     updateShader.render(particleDataFbo,
-                        field1Texture, field2Texture,
-                        field1ValueOffset, field2ValueOffset,
-                        field1MultiplierParameter, field2MultiplierParameter,
-                        velocityDampingParameter, forceMultiplierParameter,
-                        maxVelocityParameter, jitterStrengthParameter, jitterSmoothingParameter);
+                        field1Texture,
+                        field2Texture,
+                        field1ValueOffset,
+                        field2ValueOffset,
+                        getField1MultiplierEffective(),
+                        getField2MultiplierEffective(),
+                        getVelocityDampingEffective(),
+                        getForceMultiplierEffective(),
+                        getMaxVelocityEffective(),
+                        getJitterStrengthEffective(),
+                        getJitterSmoothingEffective());
   } else if (field1Texture.isAllocated()) {
     updateShader.render(particleDataFbo,
-                        field1Texture, emptyFieldTexture,
-                        field1ValueOffset, 0.0f,
-                        field1MultiplierParameter, 0.0f,
-                        velocityDampingParameter, forceMultiplierParameter,
-                        maxVelocityParameter, jitterStrengthParameter, jitterSmoothingParameter);
+                        field1Texture,
+                        emptyFieldTexture,
+                        field1ValueOffset,
+                        0.0f,
+                        getField1MultiplierEffective(),
+                        0.0f,
+                        getVelocityDampingEffective(),
+                        getForceMultiplierEffective(),
+                        getMaxVelocityEffective(),
+                        getJitterStrengthEffective(),
+                        getJitterSmoothingEffective());
   }
 }
 
 void ParticleField::draw(ofFbo& foregroundFbo, bool smallParticles) {
-  float particleSize = smallParticles ? smallParticleSize() : particleSizeParameter.get();
-  drawShader.render(mesh, foregroundFbo, particleDataFbo, particleSize, speedThresholdParameter);
+  float particleSize = smallParticles ? smallParticleSize() : getParticleSizeEffective();
+  drawShader.render(mesh, foregroundFbo, particleDataFbo, particleSize, getSpeedThresholdEffective());
 }
 
 void ParticleField::onLn2ParticleCountChanged(float& value) {
@@ -195,15 +286,15 @@ ofParameterGroup& ParticleField::getParameterGroup() {
 void ParticleField::updateRandomColorBlocks(int numBlocks, int blockSize, std::function<ofFloatColor(size_t)> colorFunc) {
   auto& colors = mesh.getColors();
   size_t totalParticles = colors.size();
-  
+
   for (int i = 0; i < numBlocks; i++) {
     size_t blockStart = (size_t)(ofRandom(totalParticles / blockSize)) * blockSize;
-    
+
     for (int j = 0; j < blockSize && (blockStart + j) < totalParticles; j++) {
       colors[blockStart + j] = colorFunc(blockStart + j);
     }
   }
-  
+
   mesh.updateVbo();
 }
 
--- src/ParticleField.h
+++ src/ParticleField.h
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <functional>
-#include "ofTexture.h"
-#include "ofVboMesh.h"
-#include "PingPongFbo.h"
+#include <optional>
+
 #include "DrawShader.h"
-#include "UpdateShader.h"
 #include "InitShader.h"
-#include "ofxGui.h"
+#include "PingPongFbo.h"
+#include "UpdateShader.h"
+#include "ofMain.h"
 
 namespace ofxParticleField {
 
@@ -16,8 +16,26 @@ namespace ofxParticleField {
 // or an ofFbo reference, whose texture is copied into fieldFbo.
 class ParticleField {
 public:
+  struct ParameterOverrides {
+    std::optional<float> velocityDamping;
+    std::optional<float> forceMultiplier;
+    std::optional<float> maxVelocity;
+    std::optional<float> particleSize;
+    std::optional<float> jitterStrength;
+    std::optional<float> jitterSmoothing;
+    std::optional<float> speedThreshold;
+    std::optional<float> minWeight;
+    std::optional<float> maxWeight;
+    std::optional<float> field1Multiplier;
+    std::optional<float> field2Multiplier;
+  };
+
   ParticleField();
   void setup(ofFloatColor particleColor, float field1ValueOffset, float field2ValueOffset);
+
+  void setParameterOverrides(const ParameterOverrides& overrides);
+  void clearParameterOverrides();
+
   void resizeParticles(int newApproxNumParticles);
   void update();
   float smallParticleSize() const { return std::min(particleSizeParameter / 12.0f, 1.0f); }
@@ -25,9 +43,9 @@ public:
   void setField1(const ofTexture& fieldTexture);
   void setField2(const ofTexture& fieldTexture);
   void updateRandomColorBlocks(int numBlocks, int blockSize, std::function<ofFloatColor(size_t)> colorFunc);
-  
+
   int getParticleCount() const { return particleDataFbo.getWidth() * particleDataFbo.getHeight(); }
-  
+
   std::string getParameterGroupName() const { return "Particle Field"; }
   ofParameterGroup parameters;
   ofParameter<float> ln2ParticleCountParameter { "ln2ParticleCount", 14.0, 10.0, 22.0 };
@@ -43,8 +61,22 @@ public:
   ofParameter<float> field1MultiplierParameter { "field1Multiplier", 1.0, 0.0, 2.0 };
   ofParameter<float> field2MultiplierParameter { "field2Multiplier", 1.0, 0.0, 2.0 };
   ofParameterGroup& getParameterGroup();
-  
+
 private:
+  float getVelocityDampingEffective() const;
+  float getForceMultiplierEffective() const;
+  float getMaxVelocityEffective() const;
+  float getParticleSizeEffective() const;
+  float getJitterStrengthEffective() const;
+  float getJitterSmoothingEffective() const;
+  float getSpeedThresholdEffective() const;
+  float getMinWeightEffective() const;
+  float getMaxWeightEffective() const;
+  float getField1MultiplierEffective() const;
+  float getField2MultiplierEffective() const;
+
+  void updateOverrides(const ParameterOverrides& overrides);
+
   size_t numDataBuffers = 4; // position, velocity, jitter, weight
   PingPongFbo particleDataFbo;
   ofFboSettings createParticleDataFboSettings(size_t width, size_t height) const;
@@ -52,19 +84,21 @@ private:
   void calculateParticleDimensions(int approxNumParticles, size_t& outWidth, size_t& outHeight) const;
   void initializeParticleRegion(size_t x, size_t y, size_t width, size_t height);
   void onLn2ParticleCountChanged(float& value);
-  
+
   bool pendingResize = false;
   int pendingParticleCount = 0;
   float lastResizeTime = 0;
   float resizeDebounceDelay = 0.3f;
-  
+
+  ParameterOverrides parameterOverrides;
+
   ofVboMesh mesh;
   ofFloatColor particleColor;
-  
+
   DrawShader drawShader;
   UpdateShader updateShader;
   InitShader initShader;
-    
+
   float field1ValueOffset, field2ValueOffset; // -0.5 when values are [0,v]; 0.0 when values are [-v,v]
   ofTexture field1Texture, field2Texture;
   ofTexture emptyFieldTexture;
@@ -73,4 +107,4 @@ private:
 
 
 
-}
+} // namespace ofxParticleField
